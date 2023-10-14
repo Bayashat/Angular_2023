7d35ab910418b7cba2a4891da28a14cc8ea35e51
Bayashat <baiashat2002@gmail.com>
2023-10-14 22:41:06 +0600

    Added Tags bar

--- TasteBudHub/src/app/app-routing.module.ts
+++ TasteBudHub/src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'search/:searchTerm', component: HomeComponent }
-];
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent}  // localhost:4200/tag/FastFood
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
