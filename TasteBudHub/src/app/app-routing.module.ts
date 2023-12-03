79f496a098ec80cbcfa8aea73b288b1a8d8f496f
Bayashat <baiashat2002@gmail.com>
2023-12-03 13:48:51 +0600

    Created Cart page

--- TasteBudHub/src/app/app-routing.module.ts
+++ TasteBudHub/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FoodPageComponent } from './food-page/food-page.component';
+import {CartPageComponent} from "./cart-page/cart-page.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
   { path: 'tag/:tag', component: HomeComponent},  // localhost:4200/tag/FastFood
   { path: 'food/:id', component: FoodPageComponent},  // localhost:4200/food/1
-]; 
+  {path: 'cart-page', component: CartPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
